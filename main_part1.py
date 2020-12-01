94de25392b9f08d23908586ed5ab6837120af6c4
Axel RANDOLPH <randolph.axel@yahoo.fr.com>
2020-12-01 16:10:07 +0100

    lisa in gray levels

---
+++ main_part1.py
@@ -0,0 +1,10 @@
+import cv2
+
+img = cv2.imread('Images2020/lisa.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('image', img)
+cv2.imshow('gray', gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
