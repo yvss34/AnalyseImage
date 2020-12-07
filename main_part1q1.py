45350574d028bb37006cd1903cf7a581fe75814d
Axel RANDOLPH <randolph.axel@yahoo.fr.com>
2020-12-07 14:59:05 +0100

    part1 q1 done

--- main_part1q1.py
+++ main_part1q1.py
@@ -1,10 +1,16 @@
 import cv2
+from matplotlib import pyplot as plt
 
 img = cv2.imread('Images2020/lisa.png')
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
 
 cv2.imshow('image', img)
 cv2.imshow('gray', gray)
+
+plt.plot(hist, color='black')
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
